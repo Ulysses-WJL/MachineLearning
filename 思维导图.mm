48221052970868d236e6386bcf6db2f3fb127e4a
Ulysses <ulysses1171@163.com>
2019-12-14 19:56:16 +0800

    LASSO, LARS 思维导图帮助学习

---
+++ 思维导图.mm
@@ -0,0 +1,46 @@
+
+<map version="0.9.0">
+    <node CREATED="0" ID="09eeaec75e064a21b6aeb8f601b9fa7c" MODIFIED="0" TEXT="思维导图">
+        <richcontent TYPE="NOTE">
+            <html>
+                <head></head>
+                <body>&lt;p&gt;设计依据&lt;/p&gt;&lt;ul&gt;&lt;li&gt;明确输入输出任务&lt;/li&gt;&lt;li&gt;将信息压缩成知识&lt;/li&gt;&lt;li&gt;例子重塑大脑连接&lt;/li&gt;&lt;li&gt;二阶知识拆分&lt;/li&gt;&lt;/ul&gt;</body>
+            </html>
+        </richcontent>
+        <node CREATED="0" ID="eb0180b9846d44b7ada44e5fef36987f" MODIFIED="0" POSITION="right" TEXT="​使用技巧">
+            <node CREATED="0" ID="ee39ca2fde1f42379a5d438603db178e" MODIFIED="0" POSITION="right" TEXT="​使用误区">
+                <node CREATED="0" ID="42ec317a5db1464fa6cdb838fd4c21db" MODIFIED="0" POSITION="right" TEXT="​知识间不独立"/>
+                <node CREATED="0" ID="ff52c08cbc7f40d5ab4ff02690516875" MODIFIED="0" POSITION="right" TEXT="​没有验证更新"/>
+                <node CREATED="0" ID="8c0fce680aa240bb8a25d641f016b5a9" MODIFIED="0" POSITION="right" TEXT="​..."/>
+            </node>
+            <node CREATED="0" ID="8b69ac1e6567460096cdcd7f2e0fd2a1" MODIFIED="0" POSITION="right" TEXT="​工具的使用"/>
+            <node CREATED="0" ID="1660706859114beca8f138a39dab332c" MODIFIED="0" POSITION="right" TEXT="​知识命名">
+                <node CREATED="0" ID="4871f36e00e54bc0b0e12e0771810f45" MODIFIED="0" POSITION="right" TEXT="​定语名词 如 分类的问题"/>
+                <node CREATED="0" ID="09b6c1fb4b084a3da1ce2646d1e5dc5e" MODIFIED="0" POSITION="right" TEXT="​动宾"/>
+            </node>
+        </node>
+        <node CREATED="0" ID="4a90309e74c24175ac0d8ad24c17ff10" MODIFIED="0" POSITION="right" TEXT="​使用步骤">
+            <node CREATED="0" ID="0a0a48b2fc2a4c7a9bfce260e185b5a5" MODIFIED="0" POSITION="right" TEXT="​搜集例子"/>
+            <node CREATED="0" ID="7fd82935ac0246b4a03ac0a57ba069e0" MODIFIED="0" POSITION="right" TEXT="​自我提问">
+                <node CREATED="0" ID="14c9aa1f7d7a4aafaee00c73f27a16eb" MODIFIED="0" POSITION="right" TEXT="​一阶知识:变量与变量之间关系">
+                    <node CREATED="0" ID="7b291992f324424497173d59235332fe" MODIFIED="0" POSITION="right" TEXT="​回归">
+                        <node CREATED="0" ID="1bf6a4eb0d3b47d3a50fff12774c4599" MODIFIED="0" POSITION="right" TEXT="​它的目的"/>
+                        <node CREATED="0" ID="b22b92145045440d996faf77bc1537ed" MODIFIED="0" POSITION="right" TEXT="​如何达成 "/>
+                    </node>
+                    <node CREATED="0" ID="cef30ffdf6ea4d43be55c38b98efc46b" MODIFIED="0" POSITION="right" TEXT="​分类: ">
+                        <node CREATED="0" ID="dea95970cd1e447a934f94ddb3c09565" MODIFIED="0" POSITION="right" TEXT="​是什么"/>
+                        <node CREATED="0" ID="e8664b537a7642ec81a0bdff9d21f877" MODIFIED="0" POSITION="right" TEXT="​为什么是"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="099448b395a04bcda494f7f1099faffc" MODIFIED="0" POSITION="right" TEXT="​二阶知识:知识与知识">
+                    <node CREATED="0" ID="4a20e8a86f814d13ac55e723ecaa3254" MODIFIED="0" POSITION="right" TEXT="​组合关系">
+                        <node CREATED="0" ID="a6d81471f0d7445bb1dd592966029cf9" MODIFIED="0" POSITION="right" TEXT="​它有什么组成"/>
+                    </node>
+                    <node CREATED="0" ID="db68573ee19d4f93be64c52f309c6161" MODIFIED="0" POSITION="right" TEXT="​执行步骤">
+                        <node CREATED="0" ID="e341084dc8b54835a62de1e2aafc667f" MODIFIED="0" POSITION="right" TEXT="​先后顺序"/>
+                    </node>
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
